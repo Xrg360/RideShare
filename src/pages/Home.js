32058a4d7b61a192e0d415d3a39ecce21151c752
rohit george <code.mastermind.07@gmail.com>
2023-09-23 10:01:37 +0530

    added navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,15 @@ const Home = () => {
   return (
     <div>
       <Navbar/>
+      <div className="container landing-container">
+      <div className="row">
+        <div className="col-md-12 text-center mt-5">
+          <h1>Welcome to Our Landing Page</h1>
+          <p>This is a Bootstrap-based landing page with centered text.</p>
+          <a href="#" className="btn btn-primary">Get Started</a>
+        </div>
+      </div>
+    </div>
 
     </div>
   )
